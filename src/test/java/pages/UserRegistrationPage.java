4a482db21cc926d344496657f3f3aa208a861774
Faisal Kabir <onlyfaisalkabir@gmail.com>
2025-05-03 20:56:53 +0600

    Registration test runner code update and user registration code update for user model

--- src/test/java/pages/UserRegistrationPage.java
+++ src/test/java/pages/UserRegistrationPage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import config.UserModel;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -12,16 +13,18 @@ public class UserRegistrationPage {
     List<WebElement> txtInput;
     @FindBy(id="register")
     WebElement btnRegister;
+
     public UserRegistrationPage(WebDriver driver){
         PageFactory.initElements(driver, this);
     }
-    public void userRegistration(String firstname, String lastname, String email, String password, String phoneNumber, String address){
-        txtInput.get(0).sendKeys(firstname); //firstname
-        txtInput.get(1).sendKeys(lastname); //lastname
-        txtInput.get(2).sendKeys(email);
-        txtInput.get(3).sendKeys(password); //password
-        txtInput.get(4).sendKeys(phoneNumber); //phoneNumber'
-        txtInput.get(5).sendKeys(address);
+
+    public void userRegistration(UserModel userModel){
+        txtInput.get(0).sendKeys(userModel.getFirstname()); //firstname
+        txtInput.get(1).sendKeys(userModel.getLastname()); //lastname
+        txtInput.get(2).sendKeys(userModel.getEmail());
+        txtInput.get(3).sendKeys(userModel.getPassword()); //password
+        txtInput.get(4).sendKeys(userModel.getPhonenumber()); //phoneNumber'
+        txtInput.get(5).sendKeys(userModel.getAddress()); //Address
         txtInput.get(6).click(); //select male gender
         txtInput.get(8).click(); //accept terms
         btnRegister.click();
