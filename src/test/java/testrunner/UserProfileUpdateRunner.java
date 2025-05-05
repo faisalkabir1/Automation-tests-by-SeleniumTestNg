b11c815a83673d6b0e02d0e3e5a5d0dedfef1e2a
Faisal Kabir <onlyfaisalkabir@gmail.com>
2025-05-05 12:43:20 +0600

    Added user can not login with old email test code , and previous code fixings

--- src/test/java/testrunner/UserProfileUpdateRunner.java
+++ src/test/java/testrunner/UserProfileUpdateRunner.java
@@ -5,14 +5,12 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import pages.UserLogin;
 import pages.UserProfileUpdate;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.text.ParseException;
 import java.time.Duration;
 import java.util.Properties;
 
@@ -24,9 +22,16 @@ public class UserProfileUpdateRunner extends Setup {
     UserLogin userLogin;
     UserProfileUpdate userProfileUpdate;
     String updatedEmail;
-    @Test(priority = 1, description = "Update Gmail")
-    public void updateUserEmail() throws InterruptedException, IOException, ParseException {
-
+    @Test(priority = 1, description = "Update Gmail from profile")
+    public void updateUserEmail() throws InterruptedException, IOException {
+        userLogin = new UserLogin(driver);
+        props = new Properties();
+        FileInputStream file = new FileInputStream("./src/test/resources/config.properties");
+        props.load(file);
+        String password= props.getProperty("Password");
+        String email = "fkkabir70@gmail.com";
+        userLogin.doLogin(email, password);
+        Thread.sleep(5000);
         userProfileUpdate = new UserProfileUpdate(driver);
         userProfileUpdate.btnProfileIcon.click();
         userProfileUpdate.btnProfileMenuItems.get(0).click();
@@ -37,9 +42,26 @@ public class UserProfileUpdateRunner extends Setup {
 
 
     }
+    @Test(priority = 2, description = "User can not login with old email")
+    public void userLoginOld() throws IOException, InterruptedException {
+        userLogin = new  UserLogin(driver);
+        userLogin.btnsubmit.click();
+        Thread.sleep(2000);
+        props = new Properties();
+        FileInputStream file = new FileInputStream("./src/test/resources/config.properties");
+        props.load(file);
+        String password = props.getProperty("Password");
+        userProfileUpdate = new UserProfileUpdate(driver);
+        String oldEmail = "fkkabir70@gmail.com";
+        userLogin.doLogin(oldEmail,password);
+        Thread.sleep(2000);
+        String expectedMsg = "Invalid email or password";
+        String actualMsg = userLogin.errorMsg.get(0).getText();
+        Assert.assertTrue(actualMsg.contains(expectedMsg));
+    }
 
-    @Test(priority = 2, description = "User Login for update email")
-    public void userLogin() throws IOException, InterruptedException {
+    @Test(priority = 3, description = "User Login with updated email")
+    public void userLoginNew() throws IOException, InterruptedException {
         Thread.sleep(5000);
         userLogin = new  UserLogin(driver);
         userLogin.btnsubmit.click();
@@ -54,6 +76,7 @@ public class UserProfileUpdateRunner extends Setup {
         Thread.sleep(2000);
         String expectedMsg = "User Daily Costs";
         String actualMsg = userLogin.dashboardMsg.getText();
+        System.out.println(actualMsg);
         Assert.assertTrue(actualMsg.contains(expectedMsg));
     }
 
