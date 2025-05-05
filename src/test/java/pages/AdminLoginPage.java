b00086a07d5167cf77df1703b5127c4e65fd8b46
Faisal Kabir <onlyfaisalkabir@gmail.com>
2025-05-05 19:59:52 +0600

    Login code update

--- src/test/java/pages/AdminLoginPage.java
+++ src/test/java/pages/AdminLoginPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class AdminLoginPage {
     @FindBy(id="email")
     WebElement txtEmail;
@@ -12,15 +14,25 @@ public class AdminLoginPage {
     WebElement txtPassword;
     @FindBy(tagName="button")
     WebElement btnLogin;
+    @FindBy(css = "[data-testid=AccountCircleIcon]")
+    public static WebElement btnProfileIcon;
+    @FindBy(css = "[role=menuitem]")
+    public static List<WebElement> btnProfileMenuItems;
 
     public AdminLoginPage(WebDriver driver){
         PageFactory.initElements(driver, this);
 
     }
+
+
     public void doLogin(String email, String password){
         txtEmail.sendKeys(email);
         txtPassword.sendKeys(password);
         btnLogin.click();
+    }
+    public static  void doLogout(){
+        btnProfileIcon.click();
+        btnProfileMenuItems.get(1).click();
     }
 
 }
--- src/test/java/testrunner/AdminLoginTestRunner.java
+++ src/test/java/testrunner/AdminLoginTestRunner.java
@@ -1,16 +1,69 @@
 package testrunner;
 
 import config.Setup;
+import org.apache.commons.configuration.plist.ParseException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 import pages.AdminLoginPage;
+import pages.SearchUpdatedGmail;
+import pages.UserProfileUpdate;
+
+import java.io.IOException;
+import java.time.Duration;
 
 public class AdminLoginTestRunner extends Setup {
 
-    @Test
-    public void adminLogin(){
-        AdminLoginPage loginpage = new AdminLoginPage(driver);
-        loginpage.doLogin("admin@test.com", "admin123");
+////    @Test
+////    public void adminLogin(){
+////        AdminLoginPage loginpage = new AdminLoginPage(driver);
+////        loginpage.doLogin("admin@test.com", "admin123");
+//
+//    }
 
+private AdminLoginPage adminLoginPage;
+    private SearchUpdatedGmail searchUpdatedGmail;
+    // private WebDriver driver;
+    @Test(priority = 1, description = "Admin Login")
+    public void adminLogin() throws IOException {
+
+        AdminLoginPage adminLoginPage = new   AdminLoginPage(driver);
+        if(System.getProperty("username")!=null && System.getProperty("password")!=null){
+            adminLoginPage.doLogin(System.getProperty("username"),System.getProperty("password"));
+        }
+        else{
+            adminLoginPage.doLogin("admin@test.com","admin123");
+        }
+        // Wait for the header element to be visible
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        WebElement headerElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("h2")));
+        String headerActual = headerElement.getText();
+        String headerExpected = "Admin Dashboard";
+
+        Assert.assertTrue(headerActual.contains(headerExpected));
+
+    }
+
+    @Test(priority = 2, description = "Searching updated gmail in row")
+    public void searchUpdatedGmail() throws InterruptedException {
+        String email= "onlyfaisalkabir@gmail.com";
+        searchUpdatedGmail = new SearchUpdatedGmail(driver);
+        Thread.sleep(2000);
+        searchUpdatedGmail.EmailSearching(email);
+        Thread.sleep(2000);
+        AdminLoginPage.doLogout();
+
+    }
+
+
+    @AfterClass
+    public void tearDown() {
+        // Close the browser
+        driver.quit();
     }
 
 }
