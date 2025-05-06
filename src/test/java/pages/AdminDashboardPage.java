2bdc5e15188e553a0713f446ba4677277443bb4a
Faisal Kabir <onlyfaisalkabir@gmail.com>
2025-05-06 10:06:31 +0600

    Header row add to txt , last column ignore as it is a view button.

--- src/test/java/pages/AdminDashboardPage.java
+++ src/test/java/pages/AdminDashboardPage.java
@@ -21,21 +21,27 @@ public class AdminDashboardPage {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
-
     public void exportUserDataToTextFile(String filePath) throws IOException {
         BufferedWriter writer = new BufferedWriter(new FileWriter(filePath));
 
+        // Write headers
+        List<WebElement> headerColumns = driver.findElements(By.xpath("//table/thead/tr/th"));
+        for (int i = 0; i < headerColumns.size() - 1; i++) { // ignore last column (View)
+            writer.write(headerColumns.get(i).getText() + "\t");
+        }
+        writer.newLine();
+
+        // Write user rows
         for (WebElement row : userRows) {
             List<WebElement> columns = row.findElements(By.tagName("td"));
-            if (columns.size() > 0) { // ensure it's not an empty row
-                for (WebElement col : columns) {
-                    writer.write(col.getText() + "\t");
+            if (columns.size() > 1) { // ensure not empty
+                for (int i = 0; i < columns.size() - 1; i++) { // skip last td (View)
+                    writer.write(columns.get(i).getText() + "\t");
                 }
                 writer.newLine();
             }
         }
-
         writer.close();
         System.out.println("User data exported to " + filePath);
     }
-}
+    }
